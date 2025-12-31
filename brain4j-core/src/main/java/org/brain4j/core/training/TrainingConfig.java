5e630b781cb6c5812c23e54333be5ad8f90d003c
echo <xreflection@outlook.it>
2025-12-31 01:00:21 +0100

    feat(refactor): minor refactor to TrainingConfig.java

--- brain4j-core/src/main/java/org/brain4j/core/training/TrainingConfig.java
+++ brain4j-core/src/main/java/org/brain4j/core/training/TrainingConfig.java
@@ -3,44 +3,17 @@ package org.brain4j.core.training;
 import org.brain4j.core.loss.LossFunction;
 import org.brain4j.core.training.optimizer.Optimizer;
 import org.brain4j.core.training.updater.Updater;
+import org.brain4j.core.training.updater.impl.StochasticUpdater;
 
 public record TrainingConfig(LossFunction loss, Optimizer optimizer, Updater updater) {
 
-    public static Builder builder() {
-        return new TrainingConfig.Builder();
+    public TrainingConfig(LossFunction loss, Optimizer optimizer) {
+        this(loss, optimizer, new StochasticUpdater()); // shortcut
     }
-    
+
     public TrainingConfig {
         if (loss == null) throw new IllegalArgumentException("Loss cannot be null!");
         if (optimizer == null) throw new IllegalArgumentException("Optimizer cannot be null!");
         if (updater == null) throw new IllegalArgumentException("Updater cannot be null!");
-    }
-
-    public static class Builder {
-        private LossFunction loss;
-        private Optimizer optimizer;
-        private Updater updater;
-
-        public Builder setLoss(LossFunction loss) {
-            this.loss = loss;
-            return this;
-        }
-
-        public Builder setOptimizer(Optimizer optimizer) {
-            this.optimizer = optimizer;
-            return this;
-        }
-
-        public Builder setUpdater(Updater updater) {
-            this.updater = updater;
-            return this;
-        }
-
-        public TrainingConfig build() {
-            if (loss == null) throw new IllegalStateException("Loss function has not been set");
-            if (optimizer == null) throw new IllegalStateException("Optimizer has not been set");
-            if (updater == null) throw new IllegalStateException("Updater has not been set");
-            return new TrainingConfig(loss, optimizer, updater);
-        }
     }
 }
